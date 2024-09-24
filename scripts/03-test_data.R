378f42a486c565088b7be8164155af50156dafde
Yuanchen <vincent.miao@mail.utoronto.ca>
2024-09-24 17:09:52 -0400

    Your commit message here

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,21 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Sanity check of the data
+# Author: Yuanchen Miao
+# Date: 24 September 2024
+# Contact: vicnent.miao@mail.utoronto.ca
+# License: none
+# Pre-requisites: need to have the analyzed data
+# Any other information needed? none
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+data <- read_csv("data/analysis_data/analysis_data.csv")
+
+# Test for NAs
+all(is.na(data$Breed))
+all(is.na(data$Bite_Circumstance))
+all(is.na(data$Date_of_Dangerous_Act))
