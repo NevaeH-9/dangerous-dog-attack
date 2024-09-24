6e48cb94620fda82d96d84489b02ee3ab9d4c1c2
Yuanchen <vincent.miao@mail.utoronto.ca>
2024-09-24 14:32:59 -0400

    Your commit message here

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,60 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Simulates data
+# Author: Yuanchen Miao
+# Date: 24 September 2024
+# Contact: vincent.miao@mail.utoronto.ca
+# License: none
+# Pre-requisites: none
+# Any other information needed? none
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(782)
 
+# Simulating number of dangerous act happens per day
+#Define the start and end date
+start_date <- as.Date("2017-01-01")
+end_date <- as.Date("2023-12-31")
 
+# set the number of random date I want to generate
+number_of_dates <- 100
 
+data <-
+  tibble(
+    dates = as.Date(
+      runif(
+        n = number_of_dates,
+        min = as.numeric(start_date),
+        max = as.numeric(end_date)
+      ),
+      origin = "1970-01-01"
+    ),
+    number_of_act = rpois(n = number_of_dates, lambda = 10)
+  )
+
+# Write_csv
+write_csv(data, file = "data/raw_data/simulated1.csv")
+
+# Simulates the total number of dangerous act of a breed
+dog_breeds <- c("Breed A", "Breed B", "Breed C")
+num_days <- 100 
+
+# Create an empty data frame to store results
+results <- data.frame(Day = rep(1:num_days, times = length(dog_breeds)),
+                      Breed = rep(dog_breeds, each = num_days),
+                      DangerousActs = integer(num_days * length(dog_breeds)))
+
+# Simulate the number of dangerous acts for each breed
+for (i in 1:length(dog_breeds)) {
+  results$DangerousActs[results$Breed == dog_breeds[i]] <- rpois(num_days, 10)
+}
+# Calculate total number of dangerous acts for each breed
+total <- results %>%
+  group_by(Breed) %>%
+  summarise(TotalActs = sum(DangerousActs))
+
+write_csv(total, file = "data/raw_data/simulate2.csv")
--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,28 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Yuanchen Miao
+# Date: 24 September 2024
+# Contact: vicnent.miao@mail.utoronto.ca
+# License: none
+# Pre-requisites: none
+# Any other information needed? none
 
-
-#### Workspace setup ####
+# Work space set up
 library(opendatatoronto)
-library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
+# get package
+package <- show_package("dogs-issued-dangerous-dog-orders")
 
 
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# get all resources for this package
+resources <- list_package_resources("dogs-issued-dangerous-dog-orders")
 
-         
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+
+
+write_csv(data, "data/raw_data/raw_data.csv")
